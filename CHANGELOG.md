104f2450b115a2b4a7dd946daa4deeb12dff27c4
Saket Choudhary <saketkc@gmail.com>
2026-07-17 14:19:37 +0530

    Release 0.1.0

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# Changelog
+
+## [Unreleased]
+
+## [0.1.0] - 2026-07-17
+
+- Initial release of yuj
--- src/yuj/__init__.py
+++ src/yuj/__init__.py
@@ -120,4 +120,4 @@ __all__ = [
     "weighted_split",
 ]
 
-__version__ = "0.1.0.dev0"
+__version__ = "0.1.0"
